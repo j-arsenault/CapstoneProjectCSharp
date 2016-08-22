da9a70323454b0f073b3103f1b71b0f35c7327d9
Jameson <jmarsenault@email.neit.edu>
2016-08-22 11:08:59 -0400

    Update Login, Search, Patient Info, Insurance Info, Medical History forms

--- PatientRecords/Form1.Designer.cs
+++ PatientRecords/Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlControls = new System.Windows.Forms.Panel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnAddPatient = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblFeedback = new System.Windows.Forms.Label();
+            this.txtUName = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.pnlControls.SuspendLayout();
+            this.pnlLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlControls
+            // 
+            this.pnlControls.Controls.Add(this.btnSearch);
+            this.pnlControls.Controls.Add(this.btnAddPatient);
+            this.pnlControls.Location = new System.Drawing.Point(108, 392);
+            this.pnlControls.Name = "pnlControls";
+            this.pnlControls.Size = new System.Drawing.Size(513, 157);
+            this.pnlControls.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(230, 203);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnAddPatient
+            // 
+            this.btnAddPatient.Location = new System.Drawing.Point(91, 67);
+            this.btnAddPatient.Name = "btnAddPatient";
+            this.btnAddPatient.Size = new System.Drawing.Size(108, 23);
+            this.btnAddPatient.TabIndex = 2;
+            this.btnAddPatient.Text = "Add New Patient";
+            this.btnAddPatient.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(333, 67);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(102, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search Patients";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeader.Location = new System.Drawing.Point(290, 36);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(142, 24);
+            this.lblHeader.TabIndex = 4;
+            this.lblHeader.Text = "Patient Records";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(122, 23);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 5;
+            this.lblUsername.Text = "Username:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(124, 74);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 6;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblFeedback
+            // 
+            this.lblFeedback.AutoSize = true;
+            this.lblFeedback.Location = new System.Drawing.Point(60, 157);
+            this.lblFeedback.Name = "lblFeedback";
+            this.lblFeedback.Size = new System.Drawing.Size(418, 13);
+            this.lblFeedback.TabIndex = 7;
+            this.lblFeedback.Text = "-- WARNING APPEARS HERE WHEN THE WRONG INFORMATION IS ENTERED---";
+            // 
+            // txtUName
+            // 
+            this.txtUName.Location = new System.Drawing.Point(186, 20);
+            this.txtUName.Name = "txtUName";
+            this.txtUName.Size = new System.Drawing.Size(168, 20);
+            this.txtUName.TabIndex = 8;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(186, 67);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(168, 20);
+            this.txtPwd.TabIndex = 9;
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.Controls.Add(this.btnLogin);
+            this.pnlLogin.Controls.Add(this.lblPassword);
+            this.pnlLogin.Controls.Add(this.txtUName);
+            this.pnlLogin.Controls.Add(this.lblUsername);
+            this.pnlLogin.Controls.Add(this.txtPwd);
+            this.pnlLogin.Controls.Add(this.lblFeedback);
+            this.pnlLogin.Location = new System.Drawing.Point(108, 114);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(509, 255);
+            this.pnlLogin.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(694, 620);
+            this.Controls.Add(this.pnlLogin);
+            this.Controls.Add(this.pnlControls);
+            this.Controls.Add(this.lblHeader);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.pnlControls.ResumeLayout(false);
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlControls;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnAddPatient;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblFeedback;
+        private System.Windows.Forms.TextBox txtUName;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Panel pnlLogin;
     }
 }
 
---
+++ PatientRecords/MedicalHistory.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace PatientRecords
+{
+    partial class MedicalHistory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MedicalHistory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1015, 756);
+            this.Name = "MedicalHistory";
+            this.Text = "MedicalHistory";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
